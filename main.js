e498fb661cc77b51f22c5045e21271b2ef1be414
Nick MacIntyre <nmacintyre@deverus.com>
2017-03-21 11:08:31 -0600

    player auto request songs from pandora if station is selected

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var http = require('http').createServer( (request, response) => response.end('su
 var socket = require('socket.io-client')('https://fathomless-falls-33454.herokuapp.com/')
 var {parse, findPaths} = require('./parseMusic.js')
 var omx = require('./omx.js')
-var dataStore = require('./dataStore.js')
 var Anesidora = require("anesidora");
 var env = require('dotenv').config()
 
@@ -13,36 +12,27 @@ var path = '/home/pi/Desktop/usbstick/'
 
 var pandora = new Anesidora(process.env.EMAIL, process.env.PASSWORD);
 
-pandora.login(function(err) {
+pandora.login(function(err) { //get filed music and pandora stations
 	if (err) throw err;
 	parse(findPaths(path)).then( filedMusic => {
-		dataStore.pandora = pandora
-		dataStore.addPandoraStations()
-	 	dataStore.addFiledMusic(filedMusic)
+		omx.pandora = pandora
+		omx.getPandoraStations()
+	 	omx.getFiledMusic(filedMusic)
 		console.log('server ready!');
 	})
 });
 
-dataStore.events.on('sendData', data => {
-	var info = {};
-	if (data) info = omx[data.command](data.params)
-	if(info) socketOut(info)
-	socketOut(dataStore.requestData())
-})
+var socketOut = info =>  socket.emit('server', {to: 'Pi', room: 'raspberry', info}) //emits info to client
 
-omx.events.on('songEnd', () => {
+omx.events.on('requestFinished', () => socketOut(omx.requestData()) ) // triggers after song data loads
+
+omx.events.on('songEnd', () => { // triggers on song end
 	if (omx.playing == omx.playList.length - 2) dataStore.selectStation(dataStore.station)
 	socketOut(omx.next())
 })
 
-socket.on('Piraspberry', ({obj, command, params}) => {
-	console.log('obj ' + obj + ' command ' + command + ' params ' + params);
-	var info;
-	if (obj == 'dataStore') info = dataStore[command](params)
-	else info = omx[command](params)
-	if (info) socket.emit('server', {to: 'Pi', room: 'raspberry', info})
+socket.on('Piraspberry', ({obj, command, params}) => { // listens for client commands
+	console.log('command ' + command + ' params ' + params);
+	var info = omx[command](params)
+	if ( info ) socketOut(info)
 })
-
-function socketOut(info){
-	socket.emit('server', {to: 'Pi', room: 'raspberry', info})
-}
--- omx.js
+++ omx.js
@@ -3,20 +3,59 @@ var Omx = require('node-omxplayer')
 
 class omx {
   constructor() {
+    this.pandora = null
+    this.pandoraStations = []
+    this.pandoraSelectedStation = null
+    this.pandoraStationPlayList = []
     this.player = null
-    this.volume = 3
-    this.playing = null
     this.playList = []
+    this.playing = null
+    this.nowPlaying = null
+    this.filedMusic = []
     this.events = new EventEmitter()
   }
 
-  setPlayList(playList){
-    this.playList = this.playList.concat(playList)
-    return this.next()
+  onRequestFinished(){ this.events.emit('requestFinished') }
+
+  getPandoraStations(){
+    var self = this
+    this.pandora.request("user.getStationList", function(err, stationList) {
+      self.pandoraStations = stationList.stations
+      self.events.emit('requestFinished')
+    });
+  }
+
+  getFiledMusic(filedMusic){
+    this.filedMusic = filedMusic
+    return this.filedMusic
+  }
+
+  getPandoraStationPlaylist(stationInfo){
+    let {stationId, stationName} = stationInfo
+    this.pandoraSelectedStation = {stationId, stationName}
+    var self = this
+    this.pandora.request("station.getPlaylist", {
+       "stationToken": stationId,
+       "additionalAudioUrl": "HTTP_128_MP3"
+   }, function(err, playlist) {
+       if (err) throw err;
+       self.station = stationName
+       var songs = playlist.items
+       songs.pop()
+       self.playList = self.playList.concat( songs )
+       if(!self.player) self.next()
+       else self.onRequestFinished()
+   });
+  }
+
+  requestData(){
+    return {filedMusic: this.filedMusic, pandoraStations: this.pandoraStations, pandoraSelectedStation: this.pandoraSelectedStation, playList: this.playList, nowPlayig: this.nowPlaying}
   }
 
   playSong(song){
-        var path = song.additionalAudioUrl
+    console.log('nowPlaying', song.songName);
+    this.nowPlaying = song
+    var path = song.additionalAudioUrl
     if (this.player) {
       if(this.player.running) this.player.quit()
       this.player = null
@@ -25,14 +64,15 @@ class omx {
     this.player = new Omx(path)
     var self = this
     this.player.on('close', () => self.events.emit('songEnd') )
-    return {nowPlaying: song}
+    this.onRequestFinished()
   }
 
   next(){
-    console.log('playing next');
     if (this.playing === null) this.playing = -1
     this.playing += 1
-    return this.playSong(this.playList[this.playing])
+    console.log('should i get more', this.playing == this.playList.length - 2);
+    if (this.playing == this.playList.length - 2) this.getPandoraStationPlaylist(this.pandoraSelectedStation)
+    this.playSong(this.playList[this.playing])
   }
 
 }
